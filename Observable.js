7dae8676dc22ff2e412442cffec18c31e4071c28
gaston <gastonCss_16@hotmail.com>
2018-07-10 00:16:10 -0300

    Todo Ok

--- Observable.js
+++ Observable.js
@@ -17,24 +17,6 @@ class Observable{
                 console.log("El Listener Dado no se ha encontrado")
       } 
 
-        /*const options = {
-            url: urlUnquyRest + "/notify",
-            body: {
-                artistId: artista.artistId,
-                subject: "Nuevo Album para artsta " + artista.name,
-                message: "Se ha agregado el album " + album.name + " al artista " + artista.name,
-                from: "notificadordeusuario9580@gmail.com",
-            },
-            json: true,
-          };
-          rp.post(options).then(() => {
-            console.log("Notificaciones enviadas con exitos"); 
-          }).catch((error) => {
-            if (error) {
-              console.log("Error" + error.message);
-            throw new ServerInternalError;
-            }
-          });*/
     }
 
     changeAddAlbum(artista, album){
